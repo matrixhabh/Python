5a446431dca2986608c6a5bfa8141f7579b7bef3
matrixhabh <corpseadvanced@gmail.com>
2024-09-05 15:01:31 +0530

    python basic

--- Index.py
+++ Index.py
@@ -1,3 +1,5 @@
+# """ """ or ''' ''' can be used as a comment in Python
+
 '''
 Machine learing library>> sklearn (Scikit) Including Pandas, TensorFLow, Numoy, Matplotlib, Seaborn.
 In Terminal >> importing is considered as a module {REPL} 
@@ -13,6 +15,7 @@ Print can also perform mathematical operations.
 i.e, Print(4*5) >> 20 (Output)
 """
 
+# This is a single line comment
 
 # Read and work with a file named 'word.csv' 
 # Python is used by great platforms like Google, youtube, Instagram, Facebook,Amazon , etc.
@@ -21,3 +24,15 @@ i.e, Print(4*5) >> 20 (Output)
 '''  i.e., Print(Hello/nWorld) >> Hello 
                                   World (Output)
 '''
+
+# We can also use a double quote sequence
+# i.e., Print('Hey there I\' your \"mate\"')
+
+# Print("Hey", 6, 7, sep="~", end="!") >> Hey~6~7!
+# Here end is used to add a character at the end of the string Also it can contain "null" character
+
+''' 
+To break line we can also code -> Print("Hello, 6, 7, sep="~", end="!\n")
+ Print("Hey") >> Hello~6~7!
+                 Hey (Output)
+''' 
