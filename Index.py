3d7233bab674d2e0859c11ea199943dbce55b4f9
matrixhabh <corpseadvanced@gmail.com>
2024-09-05 13:12:49 +0530

    Python Basics

---
+++ Index.py
@@ -0,0 +1,23 @@
+'''
+Machine learing library>> sklearn (Scikit) Including Pandas, TensorFLow, Numoy, Matplotlib, Seaborn.
+In Terminal >> importing is considered as a module {REPL} 
+               i.e., Read Evaluate Print Loop 
+''' 
+"""
+
+
+Print|("Hello World", 7) >> First Program 
+         >Here 'Hello World' and '7' is a string.
+
+Print can also perform mathematical operations. 
+i.e, Print(4*5) >> 20 (Output)
+"""
+
+
+# Read and work with a file named 'word.csv' 
+# Python is used by great platforms like Google, youtube, Instagram, Facebook,Amazon , etc.
+
+#  To break line in print function we use '/n' or '\n'
+'''  i.e., Print(Hello/nWorld) >> Hello 
+                                  World (Output)
+'''
