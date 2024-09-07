d05b86bd36a88b3b52fbc08355e194d360a6e428
Rishabh <rishabh918263@gmail.com>
2024-09-08 00:02:15 +0530

    Updated Module

--- If_Else_Elif_Condition.py
+++ If_Else_Elif_Condition.py
@@ -38,10 +38,11 @@ elif (num==0):
     print("num is zero")
 elif (num>0):
     if (num<=10):
-    print("num is between 1 and 10")
+        print("num is between 1 and 10")
     elif (num>10 and num<=20):
         print("num is between 11 and 20")
-    else: print("num is greater than 20")
+    else:
+        print("num is greater than 20")
 else:
     print("num is positive")
 
