9f71e5d9a4152528d2271c7515a1309a4347ef67
Rishabh <rishabh918263@gmail.com>
2024-09-08 14:00:14 +0530

    TimeStamp Project

---
+++ TimeStamp_Project.py
@@ -0,0 +1,15 @@
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+if (timestamp < '12:00:00' and timestamp > '24:00:00'):
+    print("Good Morning")
+if (timestamp > '12:00:00' and timestamp < '18:00:00'):
+    print("Good Afternoon")
+else:
+    print("Good Evening")
