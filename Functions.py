a7a39ee3a54b558be700396d1f0e67b2dff67096
Rishabh <rishabh918263@gmail.com>
2024-09-10 23:38:28 +0530

    Functions Module Updated

--- Functions.py
+++ Functions.py
@@ -55,3 +55,74 @@ calculategmean(c,d)
 # else:
 #     print("second number is greater or equal")
 isgreater(c,d)
+
+
+# Functons and Arguments
+
+
+def average(a,b):
+    print("The average is", (a+b)/2)
+
+average(4, 6)
+
+# Types of Arguments
+
+# # Default Argument
+'''
+We can provide a default value while creating a function.
+This way the function assumes a default value even 
+if a value is not provided in the function call for that argument.
+'''
+# 1. Default Arguments example
+
+def name(fname, mname = "John", lname = "Doe"):
+    print("Hello", fname, mname, lname)
+
+name("Amy", "Jacob", "Smith")
+
+
+# 2. Keyword Argument
+'''
+We can provide arguments with key = value, 
+this way the interpreter recognizes the arguments by the parameter name. 
+Hence, the the order in which the arguments are passed does not matter.
+'''
+#  Keyword Arguments example
+
+def name(fname, mname, lname):
+    print("Hello,", fname, mname, lname)
+
+name(mname = "Peter", lname = "Wesker", fname = "Jade")
+
+
+# 3. Variable length Argument 
+'''
+Sometimes we may need to pass more arguments than those defined in the actual function.
+ This can be done using variable-length arguments.
+'''
+#Variable Length Arguments example
+
+def name(*name):
+    print("Hello,", name[0], name[1], name[2])
+
+name("James", "Buchanan", "Barnes")
+
+# 4. Required Argument
+'''
+In case we don’t pass the arguments with a key = value syntax,
+then it is necessary to pass the arguments in the correct positional order
+and the number of arguments passed should match with actual function definition.
+'''
+# Requried Argument example
+
+def average(a, b, c=1):
+    print("The average is", (a+b+c)/3)
+
+average(5, 6)
+
+
+# Another case
+def average(a=1, b=9):
+    print("The average is", (a+b)/2)
+
+average(5) # It shows the average of 5 and 9
