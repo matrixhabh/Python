f226b6dcf89a97dd3344d0f3daf62d187f67e927
Rishabh <rishabh918263@gmail.com>
2024-09-11 22:50:31 +0530

    Functions Module Updated

--- Functions.py
+++ Functions.py
@@ -15,114 +15,142 @@
 
 # Simple function
 
-def calculategmean(a,b): # this is a function definition
-    gmean = (a*b)/(a+b)
-    print(gmean)
+# def calculategmean(a,b): # this is a function definition
+#     gmean = (a*b)/(a+b)
+#     print(gmean)
 
-def isgreater(a,b): # this is also a function definition
-    if (a>b):
-        print("first number is greater")
-    else:
-        print("second number is greater or equal")
+# def isgreater(a,b): # this is also a function definition
+#     if (a>b):
+#         print("first number is greater")
+#     else:
+#         print("second number is greater or equal")
 
-def islesser(a,b): # this is also a function definition
-    pass # altho this function is not defined yet
+# def islesser(a,b): # this is also a function definition
+#     pass # altho this function is not defined yet
 
-a = 10
-b = 5
-# gmean1 = (a*b)/(a+b)
-# print(gmean1)
-calculategmean(a,b)
+# a = 10
+# b = 5
+# # gmean1 = (a*b)/(a+b)
+# # print(gmean1)
+# calculategmean(a,b)
 
-# inluding more functions
+# # inluding more functions
 
-# if (a>b):
-#     print("first number is greater")
-# else:
-#     print("second number is greater or equal")
-isgreater(a,b)
+# # if (a>b):
+# #     print("first number is greater")
+# # else:
+# #     print("second number is greater or equal")
+# isgreater(a,b)
 
-c = 15
-d = 3
+# c = 15
+# d = 3
 
-# gmean2 = (c*d)/(c+d)
-# print(gmean2)
+# # gmean2 = (c*d)/(c+d)
+# # print(gmean2)
 
-calculategmean(c,d)
+# calculategmean(c,d)
 
-# if (c>d):
-#     print("first number is greater")
-# else:
-#     print("second number is greater or equal")
-isgreater(c,d)
-
-
-# Functons and Arguments
+# # if (c>d):
+# #     print("first number is greater")
+# # else:
+# #     print("second number is greater or equal")
+# isgreater(c,d)
 
 
-def average(a,b):
-    print("The average is", (a+b)/2)
-
-average(4, 6)
-
-# Types of Arguments
-
-# # Default Argument
-'''
-We can provide a default value while creating a function.
-This way the function assumes a default value even 
-if a value is not provided in the function call for that argument.
-'''
-# 1. Default Arguments example
-
-def name(fname, mname = "John", lname = "Doe"):
-    print("Hello", fname, mname, lname)
-
-name("Amy", "Jacob", "Smith")
+# # Functons and Arguments
 
 
-# 2. Keyword Argument
-'''
-We can provide arguments with key = value, 
-this way the interpreter recognizes the arguments by the parameter name. 
-Hence, the the order in which the arguments are passed does not matter.
-'''
-#  Keyword Arguments example
+# def average(a,b):
+#     print("The average is", (a+b)/2)
 
-def name(fname, mname, lname):
-    print("Hello,", fname, mname, lname)
+# average(4, 6)
 
-name(mname = "Peter", lname = "Wesker", fname = "Jade")
+# # Types of Arguments
+
+# # # Default Argument
+# '''
+# We can provide a default value while creating a function.
+# This way the function assumes a default value even 
+# if a value is not provided in the function call for that argument.
+# '''
+# # 1. Default Arguments example
+
+# def name(fname, mname = "John", lname = "Doe"):
+#     print("Hello", fname, mname, lname)
+
+# name("Amy", "Jacob", "Smith")
 
 
-# 3. Variable length Argument 
-'''
-Sometimes we may need to pass more arguments than those defined in the actual function.
- This can be done using variable-length arguments.
-'''
-#Variable Length Arguments example
+# # 2. Keyword Argument
+# '''
+# We can provide arguments with key = value, 
+# this way the interpreter recognizes the arguments by the parameter name. 
+# Hence, the the order in which the arguments are passed does not matter.
+# '''
+# #  Keyword Arguments example
 
-def name(*name):
-    print("Hello,", name[0], name[1], name[2])
+# def name(fname, mname, lname):
+#     print("Hello,", fname, mname, lname)
 
-name("James", "Buchanan", "Barnes")
+# name(mname = "Peter", lname = "Wesker", fname = "Jade")
 
-# 4. Required Argument
-'''
-In case we don’t pass the arguments with a key = value syntax,
-then it is necessary to pass the arguments in the correct positional order
-and the number of arguments passed should match with actual function definition.
-'''
-# Requried Argument example
 
-def average(a, b, c=1):
-    print("The average is", (a+b+c)/3)
+# # 3. Variable length Argument 
+# '''
+# Sometimes we may need to pass more arguments than those defined in the actual function.
+#  This can be done using variable-length arguments.
+# '''
+# #Variable Length Arguments example
 
+# def name(*name):
+#     print("Hello,", name[0], name[1], name[2])
+
+# name("James", "Buchanan", "Barnes")
+
+# # 4. Required Argument
+# '''
+# In case we don’t pass the arguments with a key = value syntax,
+# then it is necessary to pass the arguments in the correct positional order
+# and the number of arguments passed should match with actual function definition.
+# '''
+# # Requried Argument example
+
+# def average(a, b, c=1):
+#     print("The average is", (a+b+c)/3)
+
+# average(5, 6)
+
+
+# # Another case
+# def average(a=1, b=9):
+#     print("The average is", (a+b)/2)
+
+# average(5) # It shows the average of 5 and 9
+
+def average(*numbers):
+    print(type(numbers)) # <class 'tuple'>
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    print("The average is", sum/len(numbers))
 average(5, 6)
 
+# Keyword Arbitrary Arguments
 
-# Another case
-def average(a=1, b=9):
-    print("The average is", (a+b)/2)
+def name(**name):
+    print(type(name)) # <class 'dict'>
+    print("Hello,", name["fname"], name["mname"], name["lname"])
+name(fname = "James", mname = "Buchanan", lname = "Barnes")
 
-average(5) # It shows the average of 5 and 9
+# Importance of return function
+
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+        return sum / len(numbers) # if return is not used, the function will return None
+        # if there are two return statements, 
+        # the first return will be executed and the second return will not be executed
+
+c = average(5, 6, 7, 1)
+print(c)
