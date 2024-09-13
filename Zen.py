f8d106be280e0cd4dec37bd1b261d4056df8709f
Rishabh <rishabh918263@gmail.com>
2024-09-13 23:54:20 +0530

    The Zen Of Python

--- Zen.py
+++ Zen.py
@@ -1,4 +1,6 @@
-Explicit is better than implicit.
+# import this
+
+'''Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.
 Flat is better than nested.
@@ -15,6 +17,6 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
+Namespaces are one honking great idea -- let's do more of those!'''
 
 # Remember this quote from the Zen of Python ( Smily Face )
