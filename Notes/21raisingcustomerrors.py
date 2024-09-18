435ea3a85a9eaf7a3649e5c4f07cbd897431c195
Rishabh <rishabh918263@gmail.com>
2024-09-18 22:43:42 +0530

    Raising Custom Errors Module

---
+++ Notes/21raisingcustomerrors.py
@@ -0,0 +1,287 @@
+a = int(input("Enter any value between 5 and 9: "))
+
+if (a>5 or a<9):
+    raise ValueError("Value should be between 5 and 9")
+
+# Types of Errors:
+
+# 1. SyntaxError
+# 2. NameError
+# 3. TypeError
+# 4. ValueError
+# 5. IndexError
+# 6. KeyError
+# 7. AttributeError
+# 8. AssertionError
+# 9. EOFError
+# 10. LookupError
+# 11. FloatingPointError
+# 12. ZeroDivisionError
+# 13. EnvironmentError
+# 14. IOError
+# 15. OSError
+# 16. WindowsError
+# 17. VMSError
+# 18. MemoryError
+# 19. IndentationError
+# 20. TabError
+# 21. UnicodeError
+# 22. UnicodeEncodeError
+# 23. UnicodeDecodeError
+# 24. UnicodeTranslateError
+# 25. Warning
+# 26. UserWarning
+# 27. DeprecationWarning
+# 28. PendingDeprecationWarning
+# 29. SyntaxWarning
+# 30. RuntimeWarning
+# 31. FutureWarning
+# 32. ImportWarning
+# 33. UnicodeWarning
+# 34. BytesWarning
+# 35. ResourceWarning   
+
+# # SyntaxError Example
+
+# try:
+#     print("Hello World")
+# except SyntaxError:
+#     print("Syntax Error")
+
+# # NameError Example
+
+# try:
+#     print(a)
+# except NameError:
+#     print("Name Error")
+
+# # TypeError Example
+
+# try:
+#     print(a)
+# except TypeError:
+#     print("Type Error")
+
+# # ValueError Example
+
+# try:
+#     print(a)
+# except ValueError:
+#     print("Value Error")
+
+# # IndexError Example
+
+# try:
+#     print(a)
+# except IndexError:
+#     print("Index Error")
+
+# # KeyError Example
+
+# try:
+#     print(a)
+# except KeyError:
+#     print("Key Error")
+
+# # AttributeError Example
+
+# try:
+#     print(a)
+# except AttributeError:
+#     print("Attribute Error")
+
+# # AssertionError Example
+
+# try:
+#     print(a)
+# except AssertionError:
+#     print("Assertion Error")
+
+# # EOFError Example
+
+# try:
+#     print(a)
+# except EOFError:
+#     print("EOF Error")
+
+# # LookupError Example
+
+# try:
+#     print(a)
+# except LookupError:
+#     print("Lookup Error")
+
+# # FloatingPointError Example
+
+# try:
+#     print(a)
+# except FloatingPointError:
+#     print("Floating Point Error")
+
+# # ZeroDivisionError Example
+
+# try:
+#     print(a)
+# except ZeroDivisionError:
+#     print("Zero Division Error")
+
+# # EnvironmentError Example
+
+# try:
+#     print(a)
+# except EnvironmentError:
+#     print("Environment Error")
+
+# # IOError Example
+
+# try:
+#     print(a)
+# except IOError:
+#     print("IO Error")
+
+# # OSError Example
+
+# try:
+#     print(a)
+# except OSError:
+#     print("OS Error")
+
+# # WindowsError Example
+
+# try:
+#     print(a)
+# except WindowsError:
+#     print("Windows Error")
+
+# # VMSError Example
+
+# try:
+#     print(a)
+# except VMSError:
+#     print("VMS Error")
+
+# # MemoryError Example
+
+# try:
+#     print(a)
+# except MemoryError:
+#     print("Memory Error")
+
+# # IndentationError Example
+
+# try:
+#     print(a)
+# except IndentationError:
+#     print("Indentation Error")
+
+# # TabError Example
+
+# try:
+#     print(a)
+# except TabError:
+#     print("Tab Error")
+
+# # UnicodeError Example
+
+# try:
+#     print(a)
+# except UnicodeError:
+#     print("Unicode Error")
+
+# # UnicodeEncodeError Example
+
+# try:
+#     print(a)
+# except UnicodeEncodeError:
+#     print("Unicode Encode Error")
+
+# # UnicodeDecodeError Example
+
+# try:
+#     print(a)
+# except UnicodeDecodeError:
+#     print("Unicode Decode Error")
+
+# # UnicodeTranslateError Example
+
+# try:
+#     print(a)
+# except UnicodeTranslateError:
+#     print("Unicode Translate Error")
+
+# # Warning Example
+
+# try:
+#     print(a)
+# except Warning:
+#     print("Warning")
+
+# # UserWarning Example
+
+# try:
+#     print(a)
+# except UserWarning:
+#     print("User Warning")
+
+# # DeprecationWarning Example
+
+# try:
+#     print(a)
+# except DeprecationWarning:
+#     print("Deprecation Warning")
+
+# # PendingDeprecationWarning Example
+
+# try:
+#     print(a)
+# except PendingDeprecationWarning:
+#     print("Pending Deprecation Warning")
+
+# # SyntaxWarning Example
+
+# try:
+#     print(a)
+# except SyntaxWarning:
+#     print("Syntax Warning")
+
+# # RuntimeWarning Example
+
+# try:
+#     print(a)
+# except RuntimeWarning:
+#     print("Runtime Warning")
+
+# # FutureWarning Example
+
+# try:
+#     print(a)
+# except FutureWarning:
+#     print("Future Warning")
+
+# # ImportWarning Example
+
+# try:
+#     print(a)
+# except ImportWarning:
+#     print("Import Warning")
+
+# # UnicodeWarning Example
+
+# try:
+#     print(a)
+# except UnicodeWarning:
+#     print("Unicode Warning")
+
+# # BytesWarning Example
+
+# try:
+#     print(a)
+# except BytesWarning:
+#     print("Bytes Warning")
+
+# # ResourceWarning Example
+
+# try:
+#     print(a)
+# except ResourceWarning:
+#     print("Resource Warning")
