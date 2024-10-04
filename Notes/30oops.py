87c38a4fc802aaf0c389a92c7b0736f31b7d30e0
Rishabh <rishabh918263@gmail.com>
2024-10-04 23:50:57 +0530

    OOPs Module

---
+++ Notes/30oops.py
@@ -0,0 +1,66 @@
+# mainly there are two approaches that are used to write program or code.
+
+# 1). Procedural Programming
+# 2). Object-Oriented Programming
+
+'''
+One of the key features of OOP in Python is encapsulation, 
+which means that the internal state of an object is hidden and can only be accessed 
+or modified through the object's methods. This helps to protect the object's data 
+and prevent it from being modified in unexpected ways.
+
+Another key feature of OOP in Python is inheritance, 
+which allows new classes to be created that inherit the properties and methods of an existing class. 
+This allows for code reuse and makes it easy to create new classes 
+that have similar functionality to existing classes.
+'''
+
+##
+# Simple Procedural Programming-
+
+def hello():
+    print("Hello")
+
+hello()
+
+##
+# Object-Oriented Programming-
+
+# Basically helps in real world situations mostly
+
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+# rajeev.sales
+
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000 
+# vikrant.sales
+
+sales3 = 6000
+profit3 = 2000
+ad3 = 1000
+ 
+RailwayForm   ---> Class [blueprint]
+harry --> harry ki info wala form --> Object [entity]
+tom --> tom ki info wala form --> Object [entity]
+shubham -- shubham ki info wala form --> Object [entity]
+# shubham.changeName("Shubhi")
+
+'''
+Another key feature of OOP in Python is inheritance, 
+which allows new classes to be created that inherit the properties and methods of an existing class. 
+This allows for code reuse and makes it easy to create new classes 
+that have similar functionality to existing classes.
+'''
+'''
+Polymorphism is also supported in Python, which means that objects of 
+different classes can be treated as if they were objects of a common class. 
+This allows for greater flexibility in code and makes it easier to write code that can work 
+with multiple types of objects.
+'''
+
+# In summary, OOP in Python allows developers to model real-world concepts 
+# and entities using classes and objects, encapsulate data, reuse code through inheritance, 
+# and write more flexible code through polymorphism.
