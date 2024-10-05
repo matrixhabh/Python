37e5dc00ae68e32b5a6705c37f1365312ffdfc11
Rishabh <rishabh918263@gmail.com>
2024-10-05 23:54:34 +0530

    Contructor Module Updated

--- Notes/31classes&objects.py
+++ Notes/31classes&objects.py
@@ -83,3 +83,87 @@ b.occupation = "HR"
 a.info()
 b.info()
 c.info()
+
+##
+
+# Constructor 
+# (a better method)
+'''
+A constructor is a special method in a class used to create and initialize an object of a class. 
+There are different types of constructors. 
+Constructor is invoked automatically when an object of a class is created.
+'''
+'''
+A constructor is a unique function that gets called automatically when an object is created of a class. 
+The main purpose of a constructor is to initialize or assign values to the data members of that class. 
+It cannot return any value other than None.
+'''
+
+# Syntax of Constructor
+def __init__(self):
+	# initializations
+
+# Types of Constructors in Python
+
+# • Parameterized Constructor
+# • Default Constructor
+
+# (1) PC
+
+# When the constructor accepts arguments along with self, it is known as parameterized constructor.
+
+# These arguments can be used inside the class to assign the values to the data members.
+
+# e.g.,
+
+class Details:
+    def __init__(self, animal, group):
+        self.animal = animal
+        self.group = group
+
+obj1 = Details("Crab", "Crustaceans")
+print(obj1.animal, "belongs to the", obj1.group, "group.")
+
+# Output 
+
+# Crab belongs to the Crustaceans group.
+
+# (2) DC
+
+# When the constructor doesn't accept any arguments from the object and 
+# has only one argument, self, in the constructor, it is known as a Default constructor.
+
+# e.g.,
+
+class Details:
+  def __init__(self):
+    print("animal Crab belongs to Crustaceans group")
+obj1=Details()
+
+# Output
+
+animal Crab belongs to Crustaceans group
+
+##
+
+# Practice ;)
+
+class Person:
+
+  def __init__(self, name, occ):
+    print("Hey I am a person")
+    self.name = name
+    self.occ = occ
+
+  def info(self):
+    print(f"{self.name} is a {self.occ}")
+
+
+a = Person("Rishabh", "Developer")
+b = Person("Travis", "Head") 
+a.info()
+b.info()
+# print(a.name)
+# a.name = "Toliver"
+# a.occ = "Head"
+# a.info()
