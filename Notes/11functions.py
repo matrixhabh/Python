5e399683019497033a994120b0e7ddf394c43afd
Rishabh <rishabh918263@gmail.com>
2024-10-07 23:28:18 +0530

    Decorator Module Updated

--- Notes/11functions.py
+++ Notes/11functions.py
@@ -283,3 +283,62 @@ The final result is the sum of all the elements in the list, which is 15.
 It is important to note that the reduce function requires the functools module 
 to be imported in order to use it.
 '''
+
+##
+
+# "Python Decorators"
+'''
+Python decorators are a powerful and versatile tool that allow you to modify 
+the behavior of functions and methods. 
+They are a way to extend the functionality of a function or method without modifying its source code.
+'''
+'''
+A decorator is a function that takes another function as an argument 
+and returns a new function that modifies the behavior of the original function. 
+The new function is often referred to as a "decorated" function. 
+The basic syntax for using a decorator is the following:
+
+# @decorator_function
+# def my_function():
+    # pass
+
+'''
+
+# better example-
+
+def greet(fx):
+  def mfx(*args, **kwargs):
+    print("Good Morning")
+    fx(*args, **kwargs)
+    print("Thanks for using this function")
+  return mfx
+
+@greet
+def hello():
+  print("Hello world")
+
+@greet
+def add(a, b):
+  print(a+b)
+  
+# greet(hello)()
+hello()
+# greet(add)(1, 2)
+add(1, 2)
+
+
+# Practical usecase-
+
+import logging
+
+def log_function_call(func):
+    def decorated(*args, **kwargs):
+        logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+        result = func(*args, **kwargs)
+        logging.info(f"{func.__name__} returned {result}")
+        return result
+    return decorated
+
+@log_function_call
+def my_function(a, b):
+    return a + b
