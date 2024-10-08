65cdf1434489ad228bfcc4105caff5c8ef5a9189
Rishabh <rishabh918263@gmail.com>
2024-10-08 23:50:32 +0530

    Updated Getters and Setters Function

--- Notes/11functions.py
+++ Notes/11functions.py
@@ -342,3 +342,27 @@ def log_function_call(func):
 @log_function_call
 def my_function(a, b):
     return a + b
+
+# Getters and Setters Module
+
+# practice ;)
+
+class MyClass:
+  def __init__(self, value):
+      self._value = value
+    
+  def show(self):
+    print(f"Value is {self._value}")
+    
+  @property
+  def ten_value(self):
+      return 10* self._value
+    
+  @ten_value.setter
+  def ten_value(self, new_value):
+      self._value = new_value/10
+
+obj = MyClass(10)
+obj.ten_value = 67
+print(obj.ten_value)
+obj.show()
