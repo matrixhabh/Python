873b6294f1550fa04d09a61bede05e36bdc60866
Rishabh <rishabh918263@gmail.com>
2024-10-11 10:01:51 +0530

    Inheritance Module

---
+++ Notes/32inheritance.py
@@ -0,0 +1,28 @@
+# Inheritance Module
+
+# Practice ;)
+
+class Employee:
+  def __init__(self, name, id):
+    self.name = name
+    self.id = id 
+
+  def showDetails(self):
+    print(f"The name of Employee: {self.id} is {self.name}")
+
+class Programmer(Employee):
+  def showLanguage(self):
+    print("The default langauge is Python")
+
+
+e1 = Employee("Don Louis", 400)
+e1.showDetails()
+e2 = Programmer("Rishabh", 4100)
+e2.showDetails()
+e2.showLanguage()
+
+Output:
+
+The name of Employee: 400 is Don Louis
+The name of Employee: 4100 is Rishabh
+The default langauge is Python
