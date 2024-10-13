706c69d663a2fb642342fc5eae1502f1757302d6
Rishabh <rishabh918263@gmail.com>
2024-10-13 23:53:58 +0530

    Static Methods Concept Added

--- Notes/31classes&objects.py
+++ Notes/31classes&objects.py
@@ -167,3 +167,33 @@ b.info()
 # a.name = "Travis"
 # a.occ = "Head"
 # a.info()
+
+##
+
+# Static Methods
+'''
+Static methods are methods that are associated with a class and not with an object of the class. 
+They can be called directly using the class name.
+'''
+
+# Example-
+
+class Math:
+  def __init__(self, num):
+    self.num = num
+
+  def addtonum(self, n):
+    self.num = self.num +n
+    
+  @staticmethod
+  def add(a, b):
+      return a + b
+
+# result = Math.add(1, 2)
+# print(result) # Output: 3
+a = Math(5)
+print(a.num)
+a.addtonum(6)
+print(a.num)
+
+print(Math.add(7, 2)) 
