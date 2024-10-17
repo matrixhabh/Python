6fb91e7e9a22230fa638276cc5d4357ad2c41f1e
Rishabh <rishabh918263@gmail.com>
2024-10-17 17:40:39 +0530

    Methods Module Updated

--- Notes/33attributes&methods.py
+++ Notes/33attributes&methods.py
@@ -75,3 +75,86 @@ print(help(Person))
 # In conclusion, dir(), dict, and help() are useful built-in functions 
 # in Python that can be used to get information about objects. 
 # They are valuable tools for introspection and discovery.
+
+
+## 
+# Magic Dunder Methods in Python-
+
+class Employee:
+  name = "Rishabh"
+  def __len__(self):
+    i = 0
+
+    for c in self.name:
+        i = i + 1
+    return i
+
+e = Employee()
+print(e.name)
+print(len(e))
+
+# This is called Magic Method ;)
+
+# Context-
+
+# __init__ method
+'''
+The init method is a special method that is automatically invoked when you create a new instance of a class. 
+This method is responsible for setting up the object’s initial state, 
+and it is where you would typically define any instance variables that you need. 
+Also called "constructor"
+'''
+
+# __str__ and __repr__ methods
+'''
+The str and repr methods are both used to convert an object to a string representation. 
+The str method is used when you want to print out an object,
+while the repr method is used when you want to get a string representation of 
+an object that can be used to recreate the object.
+'''
+
+# __len__ method-
+'''
+The len method is used to get the length of an object. 
+This is useful when you want to be able to find the size of a data structure, such as a list or dictionary.
+'''
+
+# __call__ method-
+'''
+The call method is used to make an object callable, 
+meaning that you can pass it as a parameter to a function and it will be executed when the function is called.
+This is an incredibly powerful tool that allows you to create objects that behave like functions.
+'''
+
+# These are just a few of the many magic methods available in Python.
+
+# in main.py file:-
+from emp import Employee
+
+e = Employee("Harry")
+print(str(e))
+print(repr(e))
+# print(e.name)
+# print(len(e))
+e()
+
+# in emp.py file:-
+class Employee:
+
+  def __init__(self, name):
+    self.name = name
+
+  def __len__(self):
+    i = 0
+    for c in self.name:
+      i = i + 1
+    return i
+
+  def __str__(self):
+    return f"The name of the employee is {self.name} str"
+    
+  def __repr__(self):
+    return f"Employee('{self.name}')"
+
+  def __call__(self):
+    print("Hey I am good")
