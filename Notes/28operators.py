43f9259a5451bffc14704c10230b8a46899cd1b6
Rishabh <rishabh918263@gmail.com>
2024-10-25 22:57:03 +0530

    Walrus Operator Module

--- Notes/28operators.py
+++ Notes/28operators.py
@@ -33,3 +33,48 @@ print(a is b)  # True
 # For mutable objects such as lists and dictionaries, is and == can behave differently.
 # In general, you should use == when you want to compare the values of two objects, 
 # and use is when you want to check if two objects are the same object in memory.
+
+# Walrus operator
+'''
+The Walrus Operator is a new addition to Python 3.8 
+and allows you to assign a value to a variable within an expression. 
+This can be useful when you need to use a value multiple times in a loop, 
+but don't want to repeat the calculation.
+
+The Walrus Operator is represented by the := syntax 
+and can be used in a variety of contexts including while loops and if statements.
+'''
+
+# Example
+numbers = [1, 2, 3, 4, 5]
+
+while (n := len(numbers)) > 0:
+    print(numbers.pop())
+
+# Another
+
+# walrus operator :=
+
+# new to Python 3.8
+# assignment expression aka walrus operator
+# assigns values to variables as part of a larger expression
+
+# happy = True
+# print(happy)
+
+# print(happy := True)
+
+# foods = list()
+# while True:
+#   food = input("What food do you like?: ")
+#       if food == "quit":
+#           break
+#   foods.append(food)
+
+foods = list()
+while (food := input("What food do you like?: ")) != "quit":
+    foods.append(food)
+
+# In this example, the user input is assigned to the variable name using the Walrus Operator. 
+# The value of name is then used in the if statement to determine whether it is in the names list. 
+# If it is, the corresponding message is printed, otherwise, a different message is printed.
